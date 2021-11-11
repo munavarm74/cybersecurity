e491b46fc52b13d5db963ad20b02c9bb66e7656a
munavarm74 <munavar.mogal@gmail.com>
2021-11-11 18:53:49 +0000

    master

--- cyberfaker.py
+++ cyberfaker.py
@@ -1,1 +1,11 @@
 from faker import Faker
+
+faker = Faker()
+name = faker.name()
+age = faker.address()
+email = faker.email()
+country = faker.country()
+city = faker.city()
+
+print( " Faker values " )
+print ( name , " Age : ", age, " eMail :", email , "country :", country , " City :", city) 
